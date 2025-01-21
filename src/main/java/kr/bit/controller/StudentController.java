4aebb3139b67843b097dbd4a1a8e127a5b314e2d
juyoung <eunh9372@naver.com>
2025-01-21 12:43:20 +0900

    조회수정삭제

--- src/main/java/kr/bit/controller/StudentController.java
+++ src/main/java/kr/bit/controller/StudentController.java
@@ -1,14 +1,29 @@
 package kr.bit.controller;
 
-import kr.bit.service.StudentService;
+import kr.bit.beans.Student;
+import kr.bit.dao.StudentDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
 
     @Autowired
-    private StudentService studentService;
+    private StudentDAO studentDAO;
 
+    @GetMapping("/{name}")
+    public Student studentContent(@PathVariable("name") String name) {
+        return studentDAO.getStudentByName(name);
+    }
 
+    @PutMapping("/update")
+    public void studentUpdate(@RequestBody Student student) {
+        studentDAO.updateStudent(student.getName(), student);
+    }
+
+    @DeleteMapping("/{name}")
+    public void studentDelete(@PathVariable("name") String name) {
+        studentDAO.deleteStudent(name);
+    }
 }
--- src/main/java/kr/bit/mapper/StudentMapper.java
+++ src/main/java/kr/bit/mapper/StudentMapper.java
@@ -1,8 +1,15 @@
 package kr.bit.mapper;
 
+import kr.bit.beans.Student;
 import org.apache.ibatis.annotations.Mapper;
+
 
 @Mapper
 public interface StudentMapper {
 
+    public Student getStudentByName(String name);
+
+    public void studentUpdate(Student student);
+
+    public void studentDelete(String name);
 }
